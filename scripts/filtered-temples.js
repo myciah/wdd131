d43fe0dc353e076710d977a5cff49d8191fb6d36
myciah <rus21019@byui.edu>
2024-10-10 17:51:26 -0600

    working

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,127 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Update the element with the copyright date
+document.getElementById("currentyear").textContent = ` ${currentYear}`;
+
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text
+
+const hamburger = document.querySelector('.hamburgerM');
+    const navMenu = document.querySelector('.navM');
+    
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('hide');
+    });
+
+
+const button = document.querySelector('.button');
+button.addEventListener('click', () => {
+    navMenu.classList.toggle('hide');
+  });
+
+
+  const temples = [
+    {
+      templeName: "Aba Nigeria",
+      location: "Aba, Nigeria",
+      dedicated: "2005, August, 7",
+      area: 11500,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+      templeName: "Manti Utah",
+      location: "Manti, Utah, United States",
+      dedicated: "1888, May, 21",
+      area: 74792,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+      templeName: "Payson Utah",
+      location: "Payson, Utah, United States",
+      dedicated: "2015, June, 7",
+      area: 96630,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+      templeName: "Yigo Guam",
+      location: "Yigo, Guam",
+      dedicated: "2020, May, 2",
+      area: 6861,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+      templeName: "Washington D.C.",
+      location: "Kensington, Maryland, United States",
+      dedicated: "1974, November, 19",
+      area: 156558,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+      templeName: "Lima Perú",
+      location: "Lima, Perú",
+      dedicated: "1986, January, 10",
+      area: 9600,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+      templeName: "Mexico City Mexico",
+      location: "Mexico City, Mexico",
+      dedicated: "1983, December, 2",
+      area: 116642,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    // Add more temple objects here...
+    {
+        templeName: "Sapporo Japan Temple",
+        location: "Sapporo, Japan",
+        dedicated: "2016, August, 21",
+        area: 48480,
+        imageUrl:
+        "https://churchofjesuschristtemples.org/assets/img/temples/sapporo-japan-temple/sapporo-japan-temple-3374-main.jpg"
+      },
+      {
+        templeName: "Star Valley Wyoming Temple",
+        location: "Star Valley, Wyoming",
+        dedicated: "2016, October, 30",
+        area: 18609,
+        imageUrl:
+        "https://churchofjesuschristtemples.org/assets/img/temples/star-valley-wyoming-temple/star-valley-wyoming-temple-14973-main.jpg"
+      },
+      {
+        templeName: "Seoul Korea Temple",
+        location: "Seoul, Korea",
+        dedicated: "1985, December, 15",
+        area: 28057,
+        imageUrl:
+        "https://churchofjesuschristtemples.org/assets/img/temples/seoul-korea-temple/seoul-korea-temple-22305-main.jpg"
+      },
+      {
+        templeName: "Bangkok Thailand Temple",
+        location: "Bangkok, Thailand",
+        dedicated: "2023, October, 22",
+        area: 48525,
+        imageUrl:
+        "https://churchofjesuschristtemples.org/assets/img/temples/bangkok-thailand-temple/bangkok-thailand-temple-40037-main.jpg"
+      }
+  ];
+
+let temple = "";
+  for (let i = 0; i < temples.length; i++) {
+    // console.log(temples[i].templeName);
+    //  document.getElementById("temple-cards").innerHTML = temples[i].location;
+    temple = [temples[i].templeName, temples[i].location, temples.dedicated[i], temples.area[i]];
+    templeFormat = formattedString = ''
+    document.getElementById("temple-cards").innerHTML = templeFormat;
+
+    };
+
+//   document.getElementById("temple-cards").innerHTML = temples[0];
+  
